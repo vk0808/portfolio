efe25727b4ab1198773e1e3154fa61aca82d0481
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-09-27 16:17:04 +0530

    feat: add navbar with css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./styles.css";
+import Navbar from "./components/Header/Navbar";
+import "./styles/main.scss";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Header/links.js
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    id: 1,
+    url: "#",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "#about",
+    text: "about",
+  },
+  {
+    id: 3,
+    url: "#portfolio",
+    text: "portfolio",
+  },
+  {
+    id: 4,
+    url: "#contact",
+    text: "contact",
+  }
+];
