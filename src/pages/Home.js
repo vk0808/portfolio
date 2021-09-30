655fdc5e789fa40e0936dc17d77ab60ed68efbfb
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-09-30 18:39:34 +0530

    feat: create pages with routing, modify: skill images, navbar, theme color

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Navbar from "../components/Header/Navbar";
+import Intro from "../components/Intro/Intro";
+import About from "../components/About/About";
+import Projects from "../components/Projects/Projects";
+import "../styles/main.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div className="app">
+        <Intro />
+        <About />
+        <Projects />
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/SingleProject.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Navbar from "../components/Header/Navbar";
+import "../styles/main.scss";
+
+export default function SingleProject() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
